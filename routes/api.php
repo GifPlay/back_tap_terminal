e0c6ff5c83faba6f6f25c561eec5ff3a30d9dcb6
GifPlay <ioa_isma@hotmail.com>
2025-12-04 13:53:00 -0600

    add autorización a rutas 1 rol

--- routes/api.php
+++ routes/api.php
@@ -17,35 +17,38 @@ Route::middleware(['auth:api'])->get('/productos', [ProductoController::class, '
 
 
 Route::middleware(['auth:api'])->group(function () {
+    //Export
     Route::get('/export/productos/excel', [ProductoController::class, 'exportProductosExcel']);
     Route::get('/export/productos/pdf', [ProductoController::class, 'exportProductosPdf']);
 
     Route::get('/export/usuarios/excel', [UsuarioController::class, 'exportUsuariosExcel']);
     Route::get('/export/usuarios/pdf', [UsuarioController::class, 'exportUsuariosPdf']);
-});
 
-Route::get('/export/perfiles/excel', [PerfilController::class, 'exportPerfilExcel']);
-Route::get('/export/perfiles/pdf', [PerfilController::class, 'exportPerfilPdf']);
+    Route::get('/export/perfiles/excel', [PerfilController::class, 'exportPerfilExcel']);
+    Route::get('/export/perfiles/pdf', [PerfilController::class, 'exportPerfilPdf']);
 
 //Usuarios
-Route::get('/usuarios/{id}', [UsuarioController::class, 'show']);
-Route::post('/usuarios', [UsuarioController::class, 'store']);
-Route::put('/usuarios/{id}', [UsuarioController::class, 'update']);
-Route::delete('/usuarios/{id}', [UsuarioController::class, 'destroy']);
-Route::post('/foto/usuarios/{id}', [UsuarioController::class, 'uploadFoto']);
+    Route::get('/usuarios/{id}', [UsuarioController::class, 'show']);
+    Route::post('/usuarios', [UsuarioController::class, 'store']);
+    Route::put('/usuarios/{id}', [UsuarioController::class, 'update']);
+    Route::delete('/usuarios/{id}', [UsuarioController::class, 'destroy']);
+    Route::post('/foto/usuarios/{id}', [UsuarioController::class, 'uploadFoto']);
 
 //Productos
-Route::get('/productos/{id}', [ProductoController::class, 'show']);
-Route::post('/productos', [ProductoController::class, 'store']);
-Route::put('/productos/{id}', [ProductoController::class, 'update']);
-Route::delete('/productos/{id}', [ProductoController::class, 'destroy']);
+    Route::get('/productos/{id}', [ProductoController::class, 'show']);
+    Route::post('/productos', [ProductoController::class, 'store']);
+    Route::put('/productos/{id}', [ProductoController::class, 'update']);
+    Route::delete('/productos/{id}', [ProductoController::class, 'destroy']);
 
 //Perfiles
-Route::get('/perfiles', [PerfilController::class, 'index']);
-Route::get('/perfiles/{id}', [PerfilController::class, 'show']);
-Route::post('/perfiles', [PerfilController::class, 'store']);
-Route::put('/perfiles/{id}', [PerfilController::class, 'update']);
-Route::delete('/perfiles/{id}', [PerfilController::class, 'destroy']);
+    Route::get('/perfiles', [PerfilController::class, 'index']);
+    Route::get('/perfiles/{id}', [PerfilController::class, 'show']);
+    Route::post('/perfiles', [PerfilController::class, 'store']);
+    Route::put('/perfiles/{id}', [PerfilController::class, 'update']);
+    Route::delete('/perfiles/{id}', [PerfilController::class, 'destroy']);
+});
+
+
 
 
 
